a5561db3e0fe308fa2e0b5aba8e100e51bbc7ea9
babitionr <nhuy0164217@gmail.com>
2024-02-27 08:50:34 +0700

    27/02/2024

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,10 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {  
+      fontFamily: {
+      'Jost-Font': ['"Jost"', 'sans-serif'],
+    },},
   },
   plugins: [],
 }
