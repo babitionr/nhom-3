75e48c6dd61e12b3e92bbb82800e49dcdae8e769
danhthien1008 <tkgiongmk010@gmail.com>
2024-03-02 10:21:58 +0700

    responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,34 @@ export default {
       fontFamily: {
         "Jost-Font": ['"Jost"', "sans-serif"],
       },
+      width: {
+        "100px": "100px",
+        "400px": "25rem",
+        "460px": "28.75rem",
+      },
+      height: {
+        "150px": "9.375rem",
+        "60vh": "60vh",
+      },
+      padding: {
+        "4/5": "80%",
+      },
+      spacing: {
+        "3/2": "150%",
+        "4/5": "80%",
+      },
+      boxShadow: {
+        "custom-orange": "7px 7px 0px 0px rgba(250, 71, 41, 0.18)",
+        "custom-dark": "7px 7px 0px 0px rgba(16, 16, 16, 0.18)",
+      },
+      transitionTimingFunction: {
+        "custom-cubic": "cubic-bezier(0, 0, 0.3642, 1)",
+      },
     },
   },
-  plugins: [],
+  plugins: [
+    {
+      groupHover: {},
+    },
+  ],
 };
