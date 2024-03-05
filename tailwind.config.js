01a4d1d2b2660a375dbff700ce3a3c7b5d69696a
danhthien1008 <tkgiongmk010@gmail.com>
2024-03-05 08:29:04 +0700

    full

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,10 +9,14 @@ export default {
       width: {
         "100px": "100px",
         "400px": "25rem",
+        "90%": "90%",
+        "calc-100-minus-90": "calc(100% - 90px)",
       },
       height: {
         "150px": "9.375rem",
         "460px": "28.75rem",
+        "420px": "26.25rem",
+        "500px": "31.25rem",
         "60vh": "60vh",
       },
       padding: {
@@ -22,10 +26,14 @@ export default {
       spacing: {
         "3/2": "150%",
         "4/5": "80%",
+        "30%": "30%",
       },
       boxShadow: {
         "custom-orange": "7px 7px 0px 0px rgba(250, 71, 41, 0.18)",
         "custom-dark": "7px 7px 0px 0px rgba(16, 16, 16, 0.18)",
+      },
+      colors: {
+        "custom-color": "#e7e7e7",
       },
       transitionTimingFunction: {
         "custom-cubic": "cubic-bezier(0, 0, 0.3642, 1)",
